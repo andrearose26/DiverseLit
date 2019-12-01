d5b414e674233b2772742c6f3ed81614fdd3936a
andrearose26 <andreaa.lacson@gmail.com>
2019-12-01 14:01:03 -0500

    Updates

---
+++ precache-manifest.70ba1ee4c16bce59f8d039360c28af95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adca2f174e11f5ab7406d3305cae872c",
+    "url": "/andreaLacson-projectFive/index.html"
+  },
+  {
+    "revision": "b642525a526510fd4be7",
+    "url": "/andreaLacson-projectFive/static/css/main.e8bd1587.chunk.css"
+  },
+  {
+    "revision": "945e660bb5dc1d892d07",
+    "url": "/andreaLacson-projectFive/static/js/2.8b61db9e.chunk.js"
+  },
+  {
+    "revision": "b642525a526510fd4be7",
+    "url": "/andreaLacson-projectFive/static/js/main.04a53ae1.chunk.js"
+  },
+  {
+    "revision": "b26e09138bf9abd21da6",
+    "url": "/andreaLacson-projectFive/static/js/runtime-main.e14cd4e5.js"
+  },
+  {
+    "revision": "f86eeac14c4c7408b228885755f618c3",
+    "url": "/andreaLacson-projectFive/static/media/hero.f86eeac1.jpg"
+  }
+]);
